e6eabdbcd34904806713ca64b356af09575e4b69
Ronaldo Chiavegatti <129536792+RonaldoChiavegatti@users.noreply.github.com>
2025-10-31 09:28:17 -0300

    Delete backend directory

--- backend/services/agent_orchestrator/application/domain/knowledge.py
+++
@@ -1,8 +0,0 @@
-import uuid
-from pydantic import BaseModel
-
-
-class Knowledge(BaseModel):
-    id: uuid.UUID
-    title: str
-    content: str
--- backend/services/billing_service/application/domain/balance.py
+++
@@ -1,9 +0,0 @@
-import uuid
-from datetime import datetime
-from pydantic import BaseModel
-
-
-class UserBalance(BaseModel):
-    user_id: uuid.UUID
-    balance: int
-    last_updated_at: datetime
